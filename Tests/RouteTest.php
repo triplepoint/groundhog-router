f8e70771506644877c4949069b40d924ae0a4a7a
Jonathan Hanson <jonathan@jonathan-hanson.org>
2012-10-17 20:20:49 -0700

    the simplest unit test, more as a check that phpunit runs properly than anything really useful.
    Ignored composer.phar.

---
+++ Tests/RouteTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Groundhog\Router\Tests;
+
+use Groundhog\Router\Route;
+
+class RouteTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstantiationThrowsNoExceptions()
+    {
+        try {
+            $object = new Route(Route::ROUTE_TYPE_ABSOLUTE_PROTOCOL, 'GET', '/.*/', 'Class_Name', array(2, 1, 3), '/route/string');
+
+        } catch (Exception $e) {
+            $this->fail('Exception thrown during instantiation:'. $e->getMessage());
+
+        }
+    }
+}
